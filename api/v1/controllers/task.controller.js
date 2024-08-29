23cfdf59b13a1f73a07a9f11cadc8582f6f3ce96
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-08-29 21:21:17 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -10,7 +10,16 @@ module.exports.index = async (req, res) => {
   if(req.query.status) {
     find.status = req.query.status
   }
-  const tasks = await Task.find(find);
+  // End Filter
+
+  sort = {};
+
+  // Sort
+  if(req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue
+  }
+  // End Sort
+  const tasks = await Task.find(find).sort(sort);
   
   res.json(tasks);
 }
