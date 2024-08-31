14b38c7605770a9e057189c9a2b59ee301538de7
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-08-31 16:06:23 +0700

    1. Tính năng đăng nhập

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -38,3 +38,40 @@ module.exports.register = async (req, res) => {
   });
 
 }
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = md5(req.body.password);
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  })
+
+  if(!user) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại!"
+    });
+    return;
+  }
+
+  if(password !== user.password) {
+    res.json({
+      code: 400,
+      message: "Sai mật khẩu!"
+    });
+    return;
+  }
+
+  const token = user.token;
+  res.cookie("token", token);
+
+  res.json({
+    code: 200,
+    message: "Đăng nhập thành công!",
+    token: token
+  });
+
+}
